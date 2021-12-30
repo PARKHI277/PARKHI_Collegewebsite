3c31e504955833beef27d728b5a5593df851d8d4
Parkhi <parkhigarg277@gmail.com>
2021-12-30 12:01:13 +0530

    javascript adding

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("college");
-  var dots = document.getElementsByClassName("demo");
-  
+  var slides = document.getElementsByClassName("college");//6
   if (n > slides.length)
   {
     slideIndex = 1;
@@ -20,10 +18,8 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+  
+ 
 }
